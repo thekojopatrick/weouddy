47d5d4334d7035218d0b3a8559fbcacadf51a133
Kojo Patrick <patrickasare84@gmail.com>
2024-12-15 09:51:24 +0000

    `Update useMediaQuery hook to include getServerSnapshot function in useSyncExternalStore call`

--- src/hooks/useMediaQuery.ts
+++ src/hooks/useMediaQuery.ts
@@ -22,10 +22,9 @@ export function useMediaQuery(query: string) {
 
 	const getSnapshot = () => window.matchMedia(query).matches;
 
-	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	const getServerSnapshot = () => {
 		throw Error('useMediaQuery is a client-only hook');
 	};
 
-	return React.useSyncExternalStore(subscribe, getSnapshot);
+	return React.useSyncExternalStore(subscribe, getSnapshot, getServerSnapshot);
 }
