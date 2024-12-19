efd1a2199471ad848de2043c7d0511321625de67
Kojo Patrick <patrickasare84@gmail.com>
2024-12-19 09:56:35 +0000

    Update LandingPage layout and enhance SVG logo
    - Changed the SVG logo dimensions for better visibility.
    - Refactored LandingPage component to use a <main> tag for semantic structure.
    - Added Footer component to the LandingPage for improved navigation.

---
+++ src/components/marketing/footer.tsx
@@ -0,0 +1,45 @@
+import { Button } from '../ui/button';
+import { CopyIcon } from 'lucide-react';
+import Image from 'next/image';
+import React from 'react';
+
+const Footer = () => {
+	return (
+		<footer className='flex flex-col w-full justify-center items-center py-16 px-6 bg-black'>
+			<div className='max-w-6xl flex flex-col items-center gap-10 justify-center mb-10'>
+				<Image
+					src='/logomark-black-yellow-short.png'
+					alt='WeOuddy logo mark'
+					className='w-24 h-full object-cover'
+					width={200}
+					height={200}
+				/>
+				<div className='flex flex-col gap-6 max-w-md justify-center items-center'>
+					<div className='space-y-6 text-center'>
+						<h2 className='text-6xl font-display tracking-tight text-[#F9EDC0]'>
+							You dey outside,
+						</h2>
+						<p className='text-white'>
+							then me make we know the occasion, send location!.
+						</p>
+						<Button
+							variant='outline'
+							size={'lg'}
+							className='rounded-full w-auto'
+						>
+							<CopyIcon />
+							Contact us
+						</Button>
+					</div>
+				</div>
+			</div>
+			<div className='w-full mt-10 text-center'>
+				<span className='text-gray-300 font-display text-center'>
+					WeOuddy ©2024
+				</span>
+			</div>
+		</footer>
+	);
+};
+
+export default Footer;
--- src/components/pages/landing-page/index.tsx
+++ src/components/pages/landing-page/index.tsx
@@ -1,18 +1,19 @@
 import AboutUsSection from '@/components/marketing/about-us';
 import EventsSection from '@/components/marketing/events';
+import Footer from '@/components/marketing/footer';
 import HeroSection from '@/components/marketing/hero';
 import JoinUsSection from '@/components/marketing/join-us';
 import React from 'react';
 
 const LandingPage = () => {
 	return (
-		<div className='bg-[#FCFBF7]'>
+		<main className='bg-[#FCFBF7]'>
 			<HeroSection />
 			<AboutUsSection />
 			<JoinUsSection />
 			<EventsSection />
-			<div className='span'>Contact Us</div>
-		</div>
+			<Footer />
+		</main>
 	);
 };
 
