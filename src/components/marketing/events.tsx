70a589c16818a5736237d97e85f6daca681a5d93
Kojo Patrick <patrickasare84@gmail.com>
2024-12-19 13:49:40 +0000

    `feat: added dynamic events fetching and rendering`

--- src/components/marketing/events.tsx
+++ src/components/marketing/events.tsx
@@ -1,12 +1,35 @@
+import { getEvents } from '@/sanity/lib/queries';
 import React from 'react';
+import Link from 'next/link';
+import { type SanityDocument } from 'next-sanity';
+import { client } from '@/sanity/lib/client';
+import { formatEventDateTime } from '@/lib/formatters';
 
-const EventsSection = () => {
+const options = { next: { revalidate: 30 } };
+
+const EventsSection = async () => {
+	const events = await client.fetch<SanityDocument[]>(getEvents, {}, options);
+
 	return (
 		<section className='flex flex-col w-full justify-center items-center py-16'>
 			<div className='flex flex-col gap-6 max-w-4xl mt-6'>
 				<h2 className='text-xl font-display text-center tracking-normal'>
 					Upcoming Events
 				</h2>
+				<ul className='flex flex-col gap-y-4'>
+					{events.map((event) => {
+						const { date } = formatEventDateTime(event.date);
+						return (
+							<li className='hover:underline' key={event._id}>
+								<Link href={`/${event.title}`}>
+									<p>{date}</p>
+									<h2 className='text-xl font-semibold'>{event.title}</h2>
+									<p>{event.location}</p>
+								</Link>
+							</li>
+						);
+					})}
+				</ul>
 			</div>
 		</section>
 	);
