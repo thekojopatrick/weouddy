78cf310f398dd1fc10b6810fbaa8869ba5bfa7b4
thekojopatrick <patrickasare84@gmail.com>
2025-01-14 22:33:42 +0000

    Refactor layout.tsx to improve structure and add Google Analytics integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,11 @@
 import './styles/globals.css';
 
-import { cabinetGrotesk, geistMono, geistSans } from '@/components/ui/fonts';
+import {
+  cabinetGrotesk,
+  geistMono,
+  geistSans,
+} from '@/components/ui/fonts';
+import Script from 'next/script';
 
 import type { Metadata } from 'next';
 import { Toaster as SonnerToaster } from '@/components/ui/sonner';
@@ -10,23 +15,41 @@ import { siteMeta } from '@/config/metadata';
 export const metadata: Metadata = siteMeta;
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang='en'>
-			<body
-				className={`${geistSans.variable} ${geistMono.variable} ${cabinetGrotesk.variable} antialiased`}
-			>
-				<>{children}</>
-				<Toaster />
-				<SonnerToaster />
-				<script
-					defer
-					src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&libraries=places&callback=initMap`}
-				></script>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <head>
+        <Script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=G-CD6PSSN7E2`}
+        />
+        <Script id="google-analytics">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-CD6PSSN7E2', {
+                page_path: window.location.pathname,
+          });
+        `}
+        </Script>
+      </head>
+
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} ${cabinetGrotesk.variable} antialiased`}
+      >
+        <>{children}</>
+        <Toaster />
+        <SonnerToaster />
+        <Script
+          defer
+          src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&libraries=places`}
+        />
+      </body>
+    </html>
+  );
 }
