578cbdda021e2c3e05c37ec176f89ad9e6e81e8f
thekojopatrick <patrickasare84@gmail.com>
2025-01-17 09:06:50 +0000

    Refactor AppLayout to remove loading state and streamline rendering: Eliminated the loading indicator in AppLayout, allowing SiteHeader to render directly. This change simplifies the component structure and enhances the user experience by providing immediate access to the header and content. The overall code is cleaner and more efficient.

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,23 +1,15 @@
 'use client';
 
 import React, { ReactNode } from 'react';
-
 import { SiteHeader } from '@/components/site-header';
-
 import AuthWrapper from './_components/auth-wrapper';
-import { useAccount } from '@/hooks/account/use-account';
 
 const AppLayout = ({ children }: { children: ReactNode }) => {
-  const { loading } = useAccount();
-
   return (
     <AuthWrapper>
       <main>
-        {loading ? (
-          <div className="h-10 w-full bg-slate-50 shimmer" />
-        ) : (
-          <SiteHeader />
-        )}
+        <SiteHeader />
+
         <>{children}</>
       </main>
     </AuthWrapper>
