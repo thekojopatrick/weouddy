966d5a3b7bf3d5bae4768920bb091084ab497239
thekojopatrick <patrickasare84@gmail.com>
2025-01-20 21:02:05 +0000

    Update SidebarProvider in DashboardLayout to set defaultOpen to false: This change improves the initial state of the sidebar, enhancing user experience by preventing it from being open by default. This adjustment aligns with the overall goal of streamlining the dashboard interface.

--- src/app/(main)/dashboard/layout.tsx
+++ src/app/(main)/dashboard/layout.tsx
@@ -20,7 +20,7 @@ export default async function DashboardLayout({
   }
 
   return (
-    <SidebarProvider>
+    <SidebarProvider defaultOpen={false}>
       <AppSidebar user={session.user} />
       <SidebarInset>
         <DashboardHeader />
