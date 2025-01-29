bc6586a26a3631f727a3e2a008083b72a798dbe0
thekojopatrick <patrickasare84@gmail.com>
2025-01-29 08:26:07 +0000

    upgrade next

--- src/components/pages/landing-page/index.tsx
+++ src/components/pages/landing-page/index.tsx
@@ -6,15 +6,15 @@ import JoinUsSection from '@/components/marketing/join-us';
 import React from 'react';
 
 const LandingPage = () => {
-	return (
-		<main className='bg-[#FCFBF7]'>
-			<HeroSection />
-			<AboutUsSection />
-			<EventsSection />
-			<JoinUsSection />
-			<Footer />
-		</main>
-	);
+  return (
+    <main className="bg-[#FCFBF7]">
+      <HeroSection />
+      <AboutUsSection />
+      <EventsSection />
+      <JoinUsSection />
+      <Footer />
+    </main>
+  );
 };
 
 export default LandingPage;
