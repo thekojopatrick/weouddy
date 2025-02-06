a326bad047e899e1c247469df68395c3bf7c760e
thekojopatrick <patrickasare84@gmail.com>
2025-02-06 22:07:44 +0000

    Update event API route to use getEvent method and improve code formatting

--- src/app/api/events/[eventId]/route.ts
+++ src/app/api/events/[eventId]/route.ts
@@ -1,18 +1,18 @@
-import { EventService } from "@/server/services/event";
-import { NextResponse } from "next/server";
+import { EventService } from '@/server/services/event';
+import { NextResponse } from 'next/server';
 
 export async function GET(
   req: Request,
-  { params }: { params: { eventId: string } },
+  { params }: { params: { eventId: string } }
 ) {
   const { eventId } = await params;
 
   try {
-    const event = EventService.getBySlug(eventId);
+    const event = EventService.getEvent(eventId);
 
     return NextResponse.json(event);
   } catch (error) {
     console.error(error);
-    return new NextResponse("Error fetching posts", { status: 500 });
+    return new NextResponse('Error fetching posts', { status: 500 });
   }
 }
