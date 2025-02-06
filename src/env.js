2698b2b87ad64a0038acf9b3a9c6e28f1e38b746
thekojopatrick <patrickasare84@gmail.com>
2025-02-06 22:16:16 +0000

    Disable Resend and Redis environment variables

--- src/env.js
+++ src/env.js
@@ -1,5 +1,5 @@
-import { createEnv } from "@t3-oss/env-nextjs";
-import { z } from "zod";
+import { createEnv } from '@t3-oss/env-nextjs';
+import { z } from 'zod';
 
 export const env = createEnv({
   /**
@@ -11,16 +11,16 @@ export const env = createEnv({
     DIRECT_URL: z.string().url(),
     GOOGLE_CLIENT_SECRET: z.string().min(1),
     SUPABASE_KEY: z.string(),
-    RESEND_API_KEY: z.string().min(1),
-    REDIS_URL: z.string().url(),
+    //RESEND_API_KEY: z.string().min(1),
+    //REDIS_URL: z.string().url(),
     UPSTASH_REDIS_REST_URL: z.string().min(1).url().optional(),
     UPSTASH_REDIS_REST_TOKEN: z.string().min(1).optional(),
     HOST_NAME: z.string().min(1),
     EMAIL_FROM: z.string().min(1),
     EMAIL_SERVER_PASSWORD: z.string().min(1),
     NODE_ENV: z
-      .enum(["development", "test", "production"])
-      .default("development"),
+      .enum(['development', 'test', 'production'])
+      .default('development'),
   },
 
   /**
@@ -41,14 +41,15 @@ export const env = createEnv({
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     DIRECT_URL: process.env.DIRECT_URL,
-    NEXT_PUBLIC_GOOGLE_CLIENT_ID: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+    NEXT_PUBLIC_GOOGLE_CLIENT_ID:
+      process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
     NEXT_PUBLIC_GOOGLE_MAPS_API_KEY:
       process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
     SUPABASE_KEY: process.env.SUPABASE_KEY,
     NODE_ENV: process.env.NODE_ENV,
-    RESEND_API_KEY: process.env.RESEND_API_KEY,
-    REDIS_URL: process.env.REDIS_URL,
+    //RESEND_API_KEY: process.env.RESEND_API_KEY,
+    //REDIS_URL: process.env.REDIS_URL,
     UPSTASH_REDIS_REST_URL: process.env.UPSTASH_REDIS_REST_URL,
     UPSTASH_REDIS_REST_TOKEN: process.env.UPSTASH_REDIS_REST_TOKEN,
     HOST_NAME: process.env.HOST_NAME,
