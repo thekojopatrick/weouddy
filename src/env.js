7f941523700628ce47c2828f30bfb9437fc0667c
thekojopatrick <patrickasare84@gmail.com>
2025-02-06 21:16:29 +0000

    Enhance environment validation for Resend API key and formatting

--- src/env.js
+++ src/env.js
@@ -1,5 +1,5 @@
-import { createEnv } from "@t3-oss/env-nextjs";
-import { z } from "zod";
+import { createEnv } from '@t3-oss/env-nextjs';
+import { z } from 'zod';
 
 export const env = createEnv({
   /**
@@ -11,7 +11,7 @@ export const env = createEnv({
     DIRECT_URL: z.string().url(),
     GOOGLE_CLIENT_SECRET: z.string().min(1),
     SUPABASE_KEY: z.string(),
-    RESEND_API_KEY: z.string(),
+    RESEND_API_KEY: z.string().min(1),
     REDIS_URL: z.string().url(),
     UPSTASH_REDIS_REST_URL: z.string().min(1).url().optional(),
     UPSTASH_REDIS_REST_TOKEN: z.string().min(1).optional(),
@@ -19,8 +19,8 @@ export const env = createEnv({
     EMAIL_FROM: z.string().min(1),
     EMAIL_SERVER_PASSWORD: z.string().min(1),
     NODE_ENV: z
-      .enum(["development", "test", "production"])
-      .default("development"),
+      .enum(['development', 'test', 'production'])
+      .default('development'),
   },
 
   /**
@@ -41,7 +41,8 @@ export const env = createEnv({
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     DIRECT_URL: process.env.DIRECT_URL,
-    NEXT_PUBLIC_GOOGLE_CLIENT_ID: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+    NEXT_PUBLIC_GOOGLE_CLIENT_ID:
+      process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
     NEXT_PUBLIC_GOOGLE_MAPS_API_KEY:
       process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
