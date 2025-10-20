c51155c265be9cf700e20e87647dcfb93e98bc47
thekojopatrick <patrickasare84@gmail.com>
2025-10-20 10:27:26 +0000

    remove prisma

--- prisma/migrations/20250206003021_update_event_model/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "Event" ADD COLUMN     "city" TEXT,
-ADD COLUMN     "coordinates" JSONB,
-ADD COLUMN     "country" TEXT;
--- prisma/migrations/20250206163150_update_post_media/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "PostMedia" ALTER COLUMN "postId" DROP NOT NULL;
