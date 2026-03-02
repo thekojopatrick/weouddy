ff5916f2a593942cbcd1d1f019760e1e45cf9c4d
Kojo Patrick <patrickasare84@gmail.com>
2026-03-02 09:32:49 +0000

    fix security issue

--- src/components/location/location-modal.tsx
+++ src/components/location/location-modal.tsx
@@ -97,7 +97,7 @@ export function LocationModal({
 					const { latitude: lat, longitude: lng } = position.coords;
 					try {
 						const response = await fetch(
-							`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${'AIzaSyBiWsz7jQMjQBk4Ld3An2MnzZxqRhqNbOM'}`
+							`https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}`
 						);
 						const data = await response.json();
 						if (data.results[0]) {
